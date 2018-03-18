21f5e791488cee861a4c3ac0e8a01ef1fede4058
Jakub Czapliński <sirupgrade@gmail.com>
2018-03-18 15:33:39 +0100

    more examples

--- simple_examples/for3.go
+++ simple_examples/for3.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-
 	var i int
 
 	for {
 		if i >= 10 {
 			break
 		}
+
 		fmt.Println(i)
 		i++
 	}
---
+++ simple_examples/numbers2.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 2.3 // float64 !
+	var j float64 = 4
+
+	fmt.Printf("i: %v, j: %v, multi: %v\n", i, j, i*j)
+}
